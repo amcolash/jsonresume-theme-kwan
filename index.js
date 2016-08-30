777c85c2b480f5aa1e0b384d3518cb11010e65f6
Andrew McOlash <amcolash@gmail.com>
2016-08-30 09:49:52 -0500

    Add in conditional helpers

--- index.js
+++ index.js
@@ -194,7 +194,35 @@ function render(resume) {
 
     Handlebars.registerHelper('toClassName', function(text) {
         return text.toLowerCase().replace(/ /, '-');
-    })
+    });
+    
+    Handlebars.registerHelper({
+        eq: function (v1, v2) {
+            return v1 === v2;
+        },
+        ne: function (v1, v2) {
+            return v1 !== v2;
+        },
+        lt: function (v1, v2) {
+            return v1 < v2;
+        },
+        gt: function (v1, v2) {
+            return v1 > v2;
+        },
+        lte: function (v1, v2) {
+            return v1 <= v2;
+        },
+        gte: function (v1, v2) {
+            return v1 >= v2;
+        },
+        and: function (v1, v2) {
+            return v1 && v2;
+        },
+        or: function (v1, v2) {
+            return v1 || v2;
+        }
+    });
+    
     return Handlebars.compile(template)({
         css: css,
         resume: resume
